6cde47098c734430fdcabafb48164de32b9451ec
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-09-16 03:31:52 +0530

    chat system

--- Krishi/src/App.jsx
+++ Krishi/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';  // Import LandingPage
+import Chat from './components/Chat'
 import ProfileSetup from './ProfileSetup';  // Import ProfileSetup component
-import Profile from './Profile';
-import MyBids from './MyBids';
-import BidList from './components/BidList';
-import BidDetails from './components/BidDetails';
 import { auth } from './firebaseConfig';   // Import Firebase Auth (to initialize Firebase)
 
 function App() {
@@ -13,12 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/profile-setup" element={<ProfileSetup />} />
-        <Route path="/profile/:id" element={<Profile />} />
-        <Route path="/my-bids" element={<MyBids />} />
-        <Route path="/bid/:id" element={<BidDetails />} />
-        <Route path="/all-bids" element={<BidList />} />
+          <Route path="/" element={<Chat />} />
           
         </Routes>
       </div>
